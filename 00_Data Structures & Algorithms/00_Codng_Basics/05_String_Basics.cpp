15ff416f0fad4818524fb94480c8125f61c8c95c
64squares <67054089+SoumyadeepDey-snax@users.noreply.github.com>
2024-05-04 19:51:52 +0530

    Add files via upload

---
+++ 00_Data Structures & Algorithms/00_Codng_Basics/05_String_Basics.cpp
@@ -0,0 +1,27 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+string s = "love";
+int len = s.size();
+cout << len << endl;
+
+cout << s[len-1] << endl;
+
+s[1] = 'i';
+
+cout << s;
+
+/*
+
+4
+e
+live
+
+*/
+
+}
+

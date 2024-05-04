b19cc8f750b9185f3e6fd1eee75c30a134256eec
64squares <67054089+SoumyadeepDey-snax@users.noreply.github.com>
2024-05-04 19:45:43 +0530

    Add files via upload

---
+++ 00_Data Structures & Algorithms/00_Codng_Basics/04_Array_Basics.cpp
@@ -0,0 +1,38 @@
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+// 1 D arrays
+    
+int arr[5];
+cin >> arr[0] >> arr[1] >> arr[2] >> arr[3] >> arr[4]; // 1,2,3,4,5
+cout << arr[2]; // 3
+
+char c[3];
+cin >> c[0] >> c[1] >> c[2]; //a,b,c
+cout << c[1] << endl; // b
+
+string s[3];
+cin >> s[0] >> s[1] >> s[2]; // I love you
+cout << s[0] << s[1] << s[2] << endl; //Iloveyou
+
+// 2D arrays
+
+int M[2][2];
+cin >> M[0][0] >> M[0][1] >> M[1][0] >> M[1][1]; // 1 2 3 4
+
+cout << M[0][0] << " " << M[0][1] <<endl;
+cout << M[1][0] << " " << M[1][1] <<endl;
+/*
+
+1 2
+3 4
+
+*/
+
+
+}
+

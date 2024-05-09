3b583aa66043a7c08c16428a7b5f1ec3110de862
64squares <67054089+SoumyadeepDey-snax@users.noreply.github.com>
2024-05-09 19:50:06 +0530

    Add files via upload

---
+++ 00_Data Structures & Algorithms/01_Patterns/pattern5.cpp
@@ -0,0 +1,33 @@
+
+/*
+
+Input: ‘N’ = 3
+
+Output: 
+* * *
+* *
+*
+
+*/
+
+
+#include <iostream>
+using namespace std;
+
+void seeding(int n) {
+
+	for(int i=1; i <=n;i++){
+		for(int j=1; j<=n-i+1; j++){
+			cout << "* ";
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+
+int n;
+cin >> n;
+seeding(n);
+
+}
